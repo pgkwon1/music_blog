d794093cf950492c0d349061a23a7e035d68c6b8
Hyukjin Kwon <pgkwon1@naver.com>
2022-07-19 18:19:10 +0900

    Refactor : "자동 플레이 기능 개선"

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,21 @@
     
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.play').forEach(btn => {
-        btn.addEventListener('click', event => {
+        btn.addEventListener('click', async(event) => {
+            await stopAllFrame()
             let youtube_link = event.target.getAttribute("data-youtube")
-            let frame_index = event.target.getAttribute("data-frame-index")
+            let playlist_index = event.target.getAttribute("data-playlist-index")
             let music_index = Number(event.target.getAttribute("data-music-index"))
             let sec = Number(event.target.getAttribute("data-sec"))
-            let play_frame = document.querySelectorAll(".play_frame")[frame_index]
-            let next_music = document.querySelectorAll(".play")[music_index+1]
+            let play_frame = document.querySelector(".play_frame"+playlist_index)
+            let next_music = document.querySelectorAll('.play[data-playlist-index="'+playlist_index+'"]')[music_index+1]
             let list = event.target.parentNode.querySelectorAll('.play')
-            changeIframe(youtube_link, frame_index)
-            
+            changeIframe(youtube_link, playlist_index)
             play_frame.addEventListener('load', (event) => {
+                const timeOutId = setTimeout(";");
+                for (let i = 0 ; i < timeOutId ; i++) {
+                    clearTimeout(i); 
+                }
                 play_frame.contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
                 setTimeout(() => {
                     if (typeof next_music !== "undefined") {
@@ -26,7 +30,20 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     })
     function changeIframe(url, index) {
-        $(".play_frame:eq("+index+")").attr("src", "https://www.youtube.com/embed/"+url+"?enablejsapi=1&version=3&playerapiid=ytplayer&modestbranding=1&controls=0&showinfo=0")
+        $(".play_frame"+index).attr("src", "https://www.youtube.com/embed/"+url+"?enablejsapi=1&version=3&playerapiid=ytplayer&modestbranding=1&controls=0&showinfo=0")
         return true
+    }
+
+    async function stopAllFrame() {
+        const list = document.querySelectorAll(".play_frame")
+        list.forEach(play_frame => {
+            play_frame.contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+        })
+        const active = document.querySelectorAll(".active")
+        for (let i=0; i<active.length; i++) {
+            active[i].classList.remove("active")
+        }
+        return true
+        
     }
 })
