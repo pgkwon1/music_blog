5362b40fb529079fd77163eaa93dea1edb4d8bf2
Hyukjin Kwon <pgkwon1@naver.com>
2022-07-23 01:21:16 +0900

    refactor eslint and feat playlist delete

--- public/javascripts/playlist/index.js
+++ public/javascripts/playlist/index.js
@@ -1,8 +1,8 @@
 document.querySelectorAll('.add').forEach(btn => {
     btn.addEventListener('click', async(event) => {
-        let youtube_link = event.target.parentNode.querySelector('.youtube_link').value
-        let playlist = event.target.getAttribute('data-playlist')
-        let token = document.querySelector('[name="_csrf"]').value
+        const youtube_link = event.target.parentNode.querySelector('.youtube_link').value
+        const playlist = event.target.getAttribute('data-playlist')
+        const token = document.querySelector('[name="_csrf"]').value
         let frame_index = event.target.getAttribute('data-frame-index')
 
         if (!youtube_link) { 
@@ -16,8 +16,8 @@ document.querySelectorAll('.add').forEach(btn => {
             },
             method : 'POST',
             body: JSON.stringify({
-                youtube_link : youtube_link,
-                playlist : playlist
+                youtube_link,
+                playlist
             }),
         })
         result = await result.json()
@@ -31,7 +31,6 @@ document.querySelectorAll('.add').forEach(btn => {
 })
 document.querySelectorAll('.more').forEach(btn => {
     btn.addEventListener('click', async (event) => {
-        let list = event.target.parentNode.querySelectorAll('.hide')
         for (let i=0; i < 5; i++) {
             let hide = event.target.parentNode.querySelector('.hide')
             if (!hide) {
@@ -74,8 +73,28 @@ document.querySelectorAll('.delete').forEach(btn => {
 
 document.querySelectorAll('.playlist-delete').forEach(btn => {
     btn.addEventListener('click', async (event) => {
-        let playlist = event.target.getAttribute('data-playlist')
-        
-        let result = await fetch('/')
+        let playlist_id = event.target.getAttribute('data-playlist-id')
+        let token = document.querySelector('[name="_csrf"]').value
+
+        let result = await fetch('/playlist/delete', {
+            method : 'DELETE',
+            headers : {
+                'Content-Type': 'application/json', 
+                'X-CSRF-Token': token
+            },
+            body : JSON.stringify({
+                playlist_id : playlist_id
+            })
+        })
+        result = await result.json()
+        if (result.result === true) {
+            alert("삭제되었습니다.")
+            location.reload()
+        } else {
+            (typeof result.message === "object") 
+            ? alert("예기치 않은 오류가 발생하였습니다 관리자에게 문의해주세요.")
+            : alert(result.message)
+        }
+
     })
 })
