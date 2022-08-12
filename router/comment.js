79a8d06ca54ad287e6455c212325fc95e72342b4
Hyukjin Kwon <pgkwon1@naver.com>
2022-08-12 20:33:40 +0900

    Feat in a row comment store prevention

--- router/comment.js
+++ router/comment.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const csrf = require('csurf')
+const moment = require('moment')
 const CommentController = require('../controller/CommentsController')
 
 const csrfProtection = csrf({cookie : true})
@@ -7,14 +8,23 @@ const router = express.Router()
 
 router.post('/store', csrfProtection, async(req, res) => {
     try {
-        if (!req.body.playlistId || !req.session.user_id || !req.body.comment) {
+        const { playlistId, comment } = req.body
+        const { user_id, commentTime } = req.session
+        const now = new moment().format()
+
+        if (!playlistId || !user_id || !comment) {
             throw new Error("올바르지 않은 접근입니다.")
         }
+        if (commentTime && (now < commentTime)) {
+            throw new Error("연속적으로 댓글을 입력하실수 없습니다. 잠시후에 다시 시도해주세요")
+        }
+        
         const Comment = new CommentController({
-            playlistId : req.body.playlistId,
-            userId : req.session.user_id
+            playlistId,
+            user_id
         })
         await Comment.store(req.body.comment)
+        req.session.commentTime = new moment().add(3, 'minutes').format()
         res.status(200).send({
             result : true
         })
