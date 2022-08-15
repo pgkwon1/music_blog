85286f617e9da060b90d911e5309bdd22a43b45c
pgkwon1@naver.com <pgkwon1@naver.com>
2022-08-15 15:32:30 +0000

    Feat git push web hook

--- router/index.js
+++ router/index.js
@@ -1,7 +1,9 @@
+const env = require('dotenv').config()
 const csrf = require('csurf')
 
 const csrfProtection = csrf({cookie : true})
 const express = require('express')
+const { spawn } = require('child_process')
 
 const router = express.Router()
 const IndexController = require('../controller/indexController')
@@ -23,4 +25,25 @@ router.get('/', csrfProtection, async (req, res) => {
     }
 })
 
+router.post('/push', async (req,res) => {
+  	try {
+    		const command = process.env.GITHOOK_COMMAND
+    		const process_stream = spawn('bash')
+    		process_stream.stdin.write(command)
+    		process_stream.stdin.end()
+
+    		process_stream.on('close', code => {
+    			res.status(200).send({
+    				success : true,
+            code
+    			})
+    		})
+  	} catch (e) {
+    		res.status(200).send({
+    			success:false,
+          error : e.message
+    		})
+  	}
+
+})
 module.exports = router
