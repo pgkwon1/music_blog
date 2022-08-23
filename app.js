1bda2cebe78256fc9dfbea3891a47f714c6f79b3
Hyukjin Kwon <pgkwon1@naver.com>
2022-08-23 20:14:01 +0900

    Feat sentry

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
 const express = require('express')
 
 const app = express()
-const server = require('http').createServer(app)
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const ejs = require('ejs')
@@ -11,7 +9,7 @@ const session = require('express-session')
 const path = require('path')
 const methodOverride = require('method-override')
 const helmet = require('helmet')
-const Sentry = require('@sentry/node')
+const sentry = require('@sentry/node')
 const Tracing = require('@sentry/tracing')
 
 const indexRouter = require('./router/index')
@@ -22,30 +20,31 @@ const commentRouter = require('./router/comment')
 // const { sequelize } = require('./models')
 // sequelize.sync(
 
-    Sentry.init({
-        dsn: process.env.SENTRY_DSN,
-        integrations: [
-          // enable HTTP calls tracing
-          new Sentry.Integrations.Http({ tracing: true }),
-          // enable Express.js middleware tracing
-          new Tracing.Integrations.Express({ app }),
-        ],
-      
-        // Set tracesSampleRate to 1.0 to capture 100%
-        // of transactions for performance monitoring.
-        // We recommend adjusting this value in production
-        tracesSampleRate: 1.0,
-      });
-app.use(Sentry.Handlers.requestHandler())
-app.use(Sentry.Handlers.tracingHandler());
+sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new sentry.Integrations.Http({
+            tracing: true
+        }),
+        new Tracing.Integrations.Express({
+            app
+        }),
+    ],
+
+    tracesSampleRate: 1.0,
+});
+app.use(sentry.Handlers.requestHandler())
+app.use(sentry.Handlers.tracingHandler());
 
 app.use(helmet({
-    contentSecurityPolicy : false,
+    contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
     crossOriginResourcePolicy: false,
 }))
 
-app.use(helmet.referrerPolicy({ policy: 'strict-origin' }))
+app.use(helmet.referrerPolicy({
+    policy: 'strict-origin'
+}))
 
 const staticDir = (process.env.MODE === "DEV") ? "public" : "dist"
 app.use(express.static(staticDir))
@@ -55,11 +54,13 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 app.use(session({
-    secret : 'pgkwon1',
-    resave : false,
-    saveUninitialized : true,
-    rolling : true,
-    cookie: { maxAge : 600000 }
+    secret: 'pgkwon1',
+    resave: false,
+    saveUninitialized: true,
+    rolling: true,
+    cookie: {
+        maxAge: 600000
+    }
 }))
 app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, '/views'))
@@ -75,16 +76,16 @@ app.use('/music/', musicRouter)
 app.use('/playlist/', playlistRouter)
 app.use('/comment/', commentRouter)
 
-app.use(Sentry.Handlers.errorHandler())
+app.use(sentry.Handlers.errorHandler())
 
 app.use((req, res, next) => {
     res.status(404)
     res.render("404", {
-        title : 'PAGE NOT FOUND',
-        layout : false
+        title: 'PAGE NOT FOUND',
+        layout: false
     })
     next()
 })
-server.listen(3000, () => {
+app.listen(3000, () => {
     console.log("App Start")
 })
