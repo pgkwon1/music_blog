c3f2375cd240e474e61c47af0ac09aa44f0c84d2
Hyukjin Kwon <pgkwon1@naver.com>
2022-09-08 01:58:51 +0900

    Style : alert message and queryselector shortcut

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,21 +1,26 @@
+// query selector shortcut
+const query = document.querySelector.bind(document)
+const queryAll = document.querySelectorAll.bind(document)
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    const menuList = document.querySelectorAll(".nav-link")
+    const menuList = queryAll(".nav-link")
     const link = window.location.pathname
     menuList.forEach(menu => {
         menu.classList.remove("active")
     })
-    const activeLink = document.querySelector(".nav-link[href='"+link+"'")
+    const activeLink = query(".nav-link[href='" + link + "'")
     if (activeLink !== null) {
         activeLink.classList.add("active")
     }
 })
 
 const alertMessage = (message, type) => {
-    document.querySelector('.show-error').style.opacity = 0
-    document.querySelector('.show-message').style.opacity = 0
-    const alertElem = document.querySelector('.show-'+type)
+    query('.show-error').style.opacity = 0
+    query('.show-message').style.opacity = 0
+    const alertElem = query('.show-' + type)
     alertElem.innerHTML = message
     alertElem.style.opacity = 1
+    window.scrollTo(0, 0);
 
-} 
+}
